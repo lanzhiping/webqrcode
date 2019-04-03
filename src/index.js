376fd8da62eaea21a257e0c29f3228848a5b4804
Zhiping Lan <zhiping.lan@ge.com>
2019-04-03 11:57:32 +0800

    fixing

--- src/index.js
+++ src/index.js
@@ -1,77 +1,95 @@
 'use strict';
 
 (() => {
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
     const camerasEl = document.getElementById('cameras-selector');
     const video = document.getElementById('video');
     const canvas = document.createElement('canvas');
-    canvas.width = 640;
-    canvas.height = 480;
-    const ctx = canvas.getContext('2d');
-    const scaningInterval = null;
     const result = document.getElementById('result');
     const startBtn = document.getElementById('start');
     const stopBtn = document.getElementById('stop');
-    let cameras = [];
+    canvas.width = 640;
+    canvas.height = 480;
+    const ctx = canvas.getContext('2d');
+    let scaningInterval = null;
 
-    camerasEl.onchange = (event) => {
-        const deviceId = event.target.value;
-        turnOnVideo(deviceId)
+    function scan() {
+        const { width, height } = canvas;
+        ctx.drawImage(video, 0, 0, width, height);
+        const imageData = ctx.getImageData(0, 0, width, height);
+        const code = jsQR(imageData.data, width, height);
+
+        if (code) {
+            const displayCode = typeof code === 'object'
+                ? `code detected: ${JSON.stringify(code)}`
+                : `code detected: ${code}`;
+
+            result.innerText = displayCode;
+        }
     }
 
-    startBtn.onclick = function () {
-        if (scaningInterval) {
-            return;
-        }
-
-        scaningInterval = setInterval(() => {
-            result.innerText = 'scaning';
-            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-            const imageData = ctx.getImageData(0, 0, 640, 480);
-            const code = jsQR(imageData.data, 640, 480);
-
-            if (code) {
-                if (typeof code === 'object') {
-                    result.innerText = 'code detected: ' + JSON.stringify(code);
-                    return;
-                }
-                if (typeof code === 'string') {
-                    result.innerText = 'code detected: ' + code;
-                    return;
-                }
-                result.innerText = 'code detected: ' + code;
+    function renderCameraToVideo(deviceId) {
+        const constraints = {
+            video: {
+                width: 640,
+                height: 480,
+                facingMode: isMobile ? { exact: "environment" } : undefined,
+                deviceId
             }
-        }, 1000);
-    };
-
-    stopBtn.onclick = function () {
-        window.clearInterval(scaningInterval);
-        result.innerText = 'stop scaning';
-    };
-
-    function turnOnVideo(deviceId) {
-        navigator.mediaDevices.getUserMedia({ video: { deviceId } }).then(function (stream) {
+        }
+        navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
             video.srcObject = stream;
             video.play();
         });
     }
 
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.enumerateDevices().then((devices) => {
-            cameras = devices.filter(d => d.kind === 'videoinput');
+    function onCameraSelectorChange(event) {
+        const deviceId = event.target.value;
 
-            if (cameras.length) {
+        renderCameraToVideo(deviceId);
+    }
+
+    function onStartBtnClick() {
+        if (scaningInterval) {
+            return;
+        }
+        window.clearInterval(scaningInterval);
+        scaningInterval = setInterval(() => {
+            result.innerText = 'scaning';
+            scan();
+        }, 1000);
+    }
+
+    function onStopBtnClick() {
+        window.clearInterval(scaningInterval);
+        result.innerText = 'stop scaning';
+        scaningInterval = null;
+    }
+
+    camerasEl.onchange = onCameraSelectorChange;
+    startBtn.onclick = onStartBtnClick;
+    stopBtn.onclick = onStopBtnClick;
+
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices
+            .enumerateDevices()
+            .then((devices) => devices.filter(d => d.kind === 'videoinput'))
+            .then((cameras) => {
                 cameras.forEach((cam, index) => {
                     const camOption = document.createElement('option');
-                    camOption.innerText = cam.label || (index + 1);
+                    camOption.innerText = cam.label || (index + 1).toString();
                     camOption.setAttribute('value', cam.deviceId);
                     camerasEl.appendChild(camOption)
                 });
-
-                turnOnVideo(cameras[0].deviceId);
-            } else {
-                window.alert('No Camera Found')
-            }
-        });
+                return cameras[0];
+            })
+            .then((firstCameras) => {
+                if (firstCameras) {
+                    renderCameraToVideo(firstCameras.deviceId);
+                } else {
+                    window.alert('No Camera Found')
+                }
+            });
     }
 
 })();
